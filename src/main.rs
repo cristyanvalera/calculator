91d59744ac739d45d98ba460b732323b923fbe7b
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-03-12 20:26:54 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod calculator;
+
+use calculator::Calculator;
+
 fn main() {
-    println!("Hello, world!");
+    let tokens = Calculator::parse("2 * 2 + 48 / 4");
+    println!("{:?}", tokens);
+
+    let expr = Calculator::expression(tokens.unwrap());
+    println!("{:?}", expr);
 }
