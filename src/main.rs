c6c03c5bc105ef6f4a46a1965a11e8c41636c3f0
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-03-12 22:05:18 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 mod calculator;
 
-use calculator::Calculator;
+use calculator::{Calculator, Error};
 
-fn main() {
-    let tokens = Calculator::parse("2 * 2 + 48 / 4");
-    println!("{:?}", tokens);
-
-    let expr = Calculator::expression(tokens.unwrap());
-    println!("{:?}", expr);
-
-    let value = Calculator::evaluate(expr);
-    println!("{}", value.unwrap());
+fn main() -> Result<(), Error> {
+    loop {
+        let mut input = String::new();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let tokens = Calculator::parse(input);
+                if tokens.is_err() {
+                    eprintln!("{:?}", tokens.err().unwrap());
+                    continue;
+                }
+                let expr = Calculator::expression(tokens?);
+                if let Some(v) = Calculator::evaluate(expr) {
+                    println!("{v}");
+                }
+            },
+            Err(error) => eprintln!("Error: {error}"),
+        }
+    }
 }
