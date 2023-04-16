5860ed03896ed928b79f4401a97d3ee2529d9979
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-04-16 19:45:49 -0400

    formating

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,23 @@ use calculator::{Calculator, Error};
 fn main() -> Result<(), Error> {
     loop {
         let mut input = String::new();
+
         match std::io::stdin().read_line(&mut input) {
             Ok(_) => {
                 let tokens = Calculator::parse(input);
+
                 if tokens.is_err() {
                     eprintln!("{:?}", tokens.err().unwrap());
                     continue;
                 }
+
                 let expr = Calculator::expression(tokens?);
+
                 if let Some(v) = Calculator::evaluate(expr) {
                     println!("{v}");
                 }
             },
+
             Err(error) => eprintln!("Error: {error}"),
         }
     }
